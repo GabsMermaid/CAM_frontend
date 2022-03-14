c0df7719f7be7e31483080ac30321dda4e974145
Gabriela Martinez <gabrielamartinez@Gabrielas-MBP-2.localdomain>
2022-03-14 14:19:22 -0400

    Updating branch with bare boned dashboard

--- src/containers/Dashboard.jsx
+++ src/containers/Dashboard.jsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, {Component} from "react";
+import axios from "axios"
 import "./Dashboard.css";
 
-export default function Dashboard() {
- 
-  return (
-    <div className="Dashboard">
+class Dashboard extends Component {
+  
+  state = {
+    selectedFile: null
+  }
+  fileSelectedHandler = event => {
+    this.setState({
+      selectedFile: event.target.files[0]
+    })
+  }
 
-      <h1 className="Header"> Dashboard under construction on dev brach! </h1>
-
-    </div>
-  );
+  fileUploadHandler = () => {
+    axios.post('')
+  }
+  
+  render() {
+    return(
+      <div className="Dashboard">
+      <div className="Header">Account Dashboard</div>
+        <br />
+        <img alt="Person's image" /> 
+        <span> 
+          <b>Name</b>
+        </span> 
+        <br /> 
+        <span>Comment</span> 
+      </div> 
+    );
+  }
 }
+
+export default Dashboard;
