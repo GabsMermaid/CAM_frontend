a5e626f0ed64ead8c7e0e1b0c9f90edd403ec858
Gabriela Martinez <gabrielamartinez@Gabrielas-MBP-2.localdomain>
2022-03-15 19:49:48 -0400

    Revised logo, Dashboard w/o grid and fixed Dashboard buttons

--- src/containers/Dashboard.jsx
+++ src/containers/Dashboard.jsx
@@ -1,36 +1,108 @@
-import React, {Component} from "react";
-import axios from "axios"
-import "./Dashboard.css";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import ButtonGroup from '../components/elements/ButtonGroup.jsx';
+import Button from '../components/elements/Button';
+import "./Dashboard.css"
+//import classNames from 'classnames';
 
-class Dashboard extends Component {
-  
-  state = {
-    selectedFile: null
-  }
-  fileSelectedHandler = event => {
-    this.setState({
-      selectedFile: event.target.files[0]
-    })
-  }
+export default function MultilineTextFields() {
+  const [value, setValue] = React.useState('');
 
-  fileUploadHandler = () => {
-    axios.post('')
-  }
-  
-  render() {
-    return(
-      <div className="Dashboard">
-      <div className="Header">Account Dashboard</div>
-        <br />
-        <img alt="Person's image" /> 
-        <span> 
-          <b>Name</b>
-        </span> 
-        <br /> 
-        <span>Comment</span> 
-      </div> 
-    );
-  }
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <>
+    <div className="Header">Account Dashboard</div>
+    <Box
+      className="Dashboard"
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <div className='Text'>
+      <div>
+        <TextField
+          id="outlined-multiline-flexible"
+          label="Account Name"
+          value={value}
+          onChange={handleChange}
+          variant="filled"
+        />
+        <TextField
+          id="outlined-multiline-flexible"
+          label="Account type"
+          value={value}
+          onChange={handleChange}
+          variant="filled"
+        />
+      </div>
+      <br />
+      <div>
+        <TextField
+          id="outlined-multiline-static"
+          label="Account POC"
+          multiline
+          value={value}
+          onChange={handleChange}
+          variant="filled"
+        />
+        <TextField
+          id="outlined-multiline-static"
+          label="Email"
+          multiline
+          value={value}
+          onChange={handleChange}
+          variant="filled"
+        />
+      </div>
+      <br />
+      <div>
+        <TextField
+          id="outlined-multiline-static"
+          label=""
+          multiline
+          rows={2}
+          defaultValue="Account Notes"
+          
+        />
+      </div>
+      <br />
+      <div>
+        <TextField
+          id="outlined-multiline-static"
+          label=""
+          multiline
+          rows={2}
+          defaultValue="Deck Notes"
+        />
+      </div>
+      </div>
+    </Box>
+
+    <br />
+
+    <ButtonGroup className="Button">
+
+      <Button tag="a" color="primary" wideMobile onClick={() => window.open("")}>
+          Upload
+      </Button>
+
+      <Button tag="a" color="primary" wideMobile onClick={() => window.open("")}>
+          Save
+      </Button>
+
+      <Button tag="a" color="primary" wideMobile onClick={() => window.open("")}>
+          Send
+      </Button>
+
+    </ButtonGroup>
+
+  </>
+  );
 }
-
-export default Dashboard;
