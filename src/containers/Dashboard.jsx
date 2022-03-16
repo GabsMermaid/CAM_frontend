442b30fc46bb02ae4f518ffd8d22cb079718b823
Julian <jose.torres122@outlook.com>
2022-03-16 18:15:31 -0400

    Dashboard is actually visiible now and independent

--- src/containers/Dashboard.jsx
+++ src/containers/Dashboard.jsx
@@ -14,17 +14,30 @@ export default function Dashboard() {
     setValue(event.target.value);
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      account_name: data.get('account_name'),
+      account_type: data.get('account_type'),
+      account_poc: data.get('account_poc'),
+      email: data.get('email'),
+      account_notes: data.get('account_notes'),
+      deck_notes: data.get('deck_notes'),
+    });
+  };
+
   return (
     <>
     <div className="Header">Account Dashboard</div>
 
-    <div className="Container">
-      {/* <img src="" alt="selected" className="selected" /> */}
+    {/* <div className="Container">
+      <img src="" alt="selected" className="selected" />
       <div className="imgContainer">
         <img src="./images/La-Central-008.png" alt="post" />
       </div>
 
-    </div>
+    </div> */}
     
     <Box
       className="Dashboard"
@@ -34,65 +47,90 @@ export default function Dashboard() {
       }}
       noValidate
       autoComplete="off"
-    >
+      onSubmit={handleSubmit}
+      >
       <div className='Text'>
       <div>
         <TextField
           id="outlined-multiline-flexible"
+          name='account_name'
           label="Account Name"
           color="secondary"
-          value={value}
-          onChange={handleChange}
+          // value={value}
+          // onChange={handleChange}
           variant="filled"
+          sx={{backgroundColor:"gray", color:"white"}}
+          InputLabelProps={{style:{color:'white'}}}
+          InputProps={{style: {color:"white"}}}
         />
         <TextField
           id="outlined-multiline-flexible"
+          name='account_type'
           label="Account type"
           color="secondary"
-          value={value}
-          onChange={handleChange}
+          // value={value}
+          // onChange={handleChange}
           variant="filled"
+          sx={{backgroundColor:"gray", color:"white"}}
+          InputLabelProps={{style:{color:'white'}}}
+          InputProps={{style: {color:"white"}}}
         />
       </div>
       <br />
       <div>
         <TextField
           id="outlined-multiline-static"
+          name='account_poc'
           label="Account POC"
           color="secondary"
           multiline
-          value={value}
-          onChange={handleChange}
+          // value={value}
+          // onChange={handleChange}
           variant="filled"
+          sx={{backgroundColor:"gray", color:"white"}}
+          InputLabelProps={{style:{color:'white'}}}
+          InputProps={{style: {color:"white"}}}
         />
         <TextField
           id="outlined-multiline-static"
+          name='email'
           label="Email"
           color="secondary"
           multiline
-          value={value}
-          onChange={handleChange}
+          // value={value}
+          // onChange={handleChange}
           variant="filled"
+          sx={{backgroundColor:"gray", color:"white"}}
+          InputLabelProps={{style:{color:'white'}}}
+          InputProps={{style: {color:"white"}}}
         />
       </div>
       <br />
       <div>
         <TextField
           id="outlined-multiline-static"
+          name='account_notes'
           label="Account Notes"
           color="secondary"
           multiline
           rows={4}
           variant='filled'
+          sx={{backgroundColor:"gray", color:"white"}}
+          InputLabelProps={{style:{color:'white'}}}
+          InputProps={{style: {color:"white"}}}
           
         />
         <TextField
           id="outlined-multiline-static"
+          name='deck_notes'
           label="Deck Notes"
           color="secondary"
           multiline
           rows={4}
           variant='filled'
+          sx={{backgroundColor:"gray", color:"white"}}
+          InputLabelProps={{style:{color:'white'}}}
+          InputProps={{style: {color:"white"}}}
         />
       </div>
       </div>
